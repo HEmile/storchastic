Fixed stack overflow due to __eq__ overload
- Removed the __eq__ and __ne__ overloads to make sure storch.Tensor in list behaves as expected, and does not open an infinite loop

test/test.py
@@ -9,11 +9,6 @@ mu_prior = torch.tensor([2., -3.], requires_grad=True)
 theta = torch.tensor([4., 5])
 
 
-@storch.deterministic
-def add(s1, s2, mu):
-    return s2 + s1 * mu
-
-
 @storch.stochastic
 def white_noise(mu):
     return storch.sample(Normal(mu, 1), method=ScoreFunction(), n=1), storch.sample(Normal(-mu, 1), n=3)
@@ -30,13 +25,9 @@ mu = storch.sample(Normal(mu_prior, 1), n=4)
 agg_v = 0.
 for i in range(2):
     s1, s2 = white_noise(mu)
-    mult1 = s2 * mu
-    add_1 = s1 + mult1
-    agg_v = add_1 + agg_v
-    # print(agg_v[..., 1])
-    print(agg_v.max(dim=-1))
-    print(agg_v.mean(dim=-1))
-    print(agg_v.view((-1, 8)))
+    print("blaaa")
+    agg_v = agg_v + s1 + s2 * mu
+    print("bloooo")
     loss(agg_v)
 
 storch.backward(debug=False, keep_grads=True)