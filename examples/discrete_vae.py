Fixed variance computation of discrete vae
Closes #37

examples/discrete_vae.py
@@ -167,19 +167,11 @@ def train(epoch):
             for i in range(10):
                 optimizer.zero_grad()
                 recon_batch, _, z = model(data)
-                loss_function(recon_batch, data)
+                storch.add_cost(loss_function(recon_batch, data), "reconstruction")
                 backward()
-                expected_grad = z.total_expected_grad()
-                grads_logits.append(expected_grad["logits"].unsqueeze(0))
+                grads_logits.append(z.grad["logits"].unsqueeze(0))
 
-            def _var(t):
-                m = torch.cat(t)
-                mean = m.mean(0)
-                squared_diff = (m - mean) ** 2
-                sse = squared_diff.sum(0)
-                return sse.mean()
-
-            variance = _var(grads_logits)
+            variance = torch.cat(grads_logits).var(0).mean()
             step = 100.0 * batch_idx / len(train_loader)
             global_step = 100 * (epoch - 1) + step
             print(

storch/method/method.py
@@ -24,7 +24,7 @@ import itertools
 class Method(ABC, torch.nn.Module):
     @staticmethod
     def _create_hook(sample: StochasticTensor, name: str):
-        accum_grads = sample._accum_grads
+        accum_grads = sample.param_grads
         del sample  # Remove from hook closure for GC reasons
 
         def hook(grad: torch.Tensor):

storch/tensor.py
@@ -550,7 +550,7 @@ class _StochasticTensorBase(Tensor):
         self._requires_grad = requires_grad
         self.n = n
         self.sampling_method = sampling_method
-        self._accum_grads = {}
+        self.param_grads = {}
         self._grad = None
 
     @property
@@ -565,39 +565,7 @@ class _StochasticTensorBase(Tensor):
 
     @property
     def grad(self):
-        return self._accum_grads
-
-    def total_expected_grad(self) -> Dict[str, torch.Tensor]:
-        r = {}
-        indices = self.plate_dim_indices()
-        for name, grad in self._accum_grads.items():
-            tensor = getattr(self.distribution, name)
-            if grad.dim() == tensor.dim():
-                r[name] = grad
-            else:
-                r[name] = grad.mean(dim=indices)
-        return r
-
-    def total_variance_grad(self) -> Dict[str, torch.Tensor]:
-        """
-        Computes the total variance on the gradient of the parameters of this distribution over all simulations .
-        :return:
-        """
-        r = {}
-        indices = self.plate_dim_indices()
-        for name, grad in self._accum_grads.items():
-            tensor = getattr(self.distribution, name)
-            if grad.dim() == tensor.dim():
-                raise ValueError(
-                    "There are no batched dimensions to take statistics over. Make sure to call backwards "
-                    "with accum_grad=True"
-                )
-            expected = grad.mean(dim=indices)
-            diff = grad - expected
-            squared_diff = diff * diff
-            sse = squared_diff.sum(dim=indices)
-            r[name] = sse.mean()
-        return r
+        return self.param_grads
 
 
 class StochasticTensor(_StochasticTensorBase):