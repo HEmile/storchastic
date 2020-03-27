Fixed RELAX bugs, automatic right-broadcasting
- Fixed many bugs with RELAX
- Implemented helpful storch.grad which wraps torch.autograd.grad for storch.Tensor
- Now automatically right-broadcasts storch tensors over event dimensions to allow easier code

examples/vae/discrete_vae.py
@@ -17,13 +17,21 @@ class DiscreteVAE(VAE):
 
     def initialize_method(self, args) -> storch.method.Method:
         if args.method == "gumbel":
-            return storch.method.GumbelSoftmax()
+            return storch.GumbelSoftmax()
         elif args.method == "gumbel_straight":
-            return storch.method.GumbelSoftmax(straight_through=True)
+            return storch.GumbelSoftmax(straight_through=True)
         elif args.method == "score":
-            return storch.method.ScoreFunction(baseline_factory=args.baseline)
+            return storch.ScoreFunction(baseline_factory=args.baseline)
         elif args.method == "expect":
-            return storch.method.Expect()
+            return storch.Expect()
+        elif args.method == "relax":
+            return storch.RELAX(in_dim=(args.latents, 10))
+        elif args.method == "rebar":
+            return storch.REBAR()
+        elif args.method == "relax_rebar":
+            return storch.RELAX(in_dim=(args.latents, 10), rebar=True)
+        else:
+            raise ValueError("Invalid method passed to program arguments.")
 
     def prior(self, shape: List[int]):
         return OneHotCategorical(probs=torch.ones(shape + [10]) / (1.0 / 10.0))

examples/vae/normal_vae.py
@@ -19,11 +19,11 @@ class NormalVAE(VAE):
 
     def initialize_method(self, args) -> storch.method.Method:
         if args.method == "reparameterization":
-            return storch.method.Reparameterization()
+            return storch.Reparameterization()
         elif args.method == "lax":
-            return storch.method.LAX(in_dim=args.latents)
+            return storch.LAX(in_dim=args.latents)
         elif args.method == "score":
-            return storch.method.ScoreFunction(baseline_factory=args.baseline)
+            return storch.ScoreFunction(baseline_factory=args.baseline)
 
     def prior(self, shape: List[int]) -> Distribution:
         return Normal(torch.zeros(shape), torch.ones(shape))

storch/storch.py
@@ -1,7 +1,9 @@
-from typing import List, Union, Optional
+from typing import List, Union, Optional, Tuple
 
 import storch
 import torch
+
+from storch import deterministic
 
 
 def _convert_indices(
@@ -123,3 +125,37 @@ def variance(
     mean = variance_plate.reduce(tensor, detach_weights=detach_weights)
     variance = ((tensor - mean) ** 2).sum(tensor.event_dim_indices())
     return reduce_plates(variance, detach_weights=detach_weights)
+
+
+def grad(
+    outputs,
+    inputs,
+    grad_outputs=None,
+    retain_graph: Optional[bool] = None,
+    create_graph: bool = False,
+    only_inputs: bool = True,
+    allow_unused: bool = False,
+) -> Tuple[storch.Tensor, ...]:
+    """
+    Helper method for computing torch.autograd.grad on storch tensors. Returns storch Tensors as well.
+    """
+    args, _, _, _ = storch.wrappers._handle_args(
+        True, False, outputs, inputs, grad_outputs
+    )
+    _outputs, _inputs, _grad_outputs = tuple(args)
+    grads = torch.autograd.grad(
+        _outputs,
+        _inputs,
+        grad_outputs=_grad_outputs,
+        retain_graph=retain_graph,
+        create_graph=create_graph,
+        only_inputs=only_inputs,
+        allow_unused=allow_unused,
+    )
+    storch_grad = []
+    for i, grad in enumerate(grads):
+        input = inputs[i]
+        storch_grad.append(
+            storch.Tensor(grad, outputs + [input], input.plates, input.name + "_grad")
+        )
+    return tuple(storch_grad)