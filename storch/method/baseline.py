Fixed reduction of Moving Average Baseline

storch/method/baseline.py
@@ -16,7 +16,10 @@ class Baseline(ABC, torch.nn.Module):
 
 
 class MovingAverageBaseline(Baseline):
-    # TODO: Moving Average baseline does not take surrounding plates into account
+    """
+    Takes the (unconditional) average over the different costs. This is not as precise as a conditional baseline.
+    """
+
     def __init__(self, exponential_decay=0.95):
         super().__init__()
         self.register_buffer("exponential_decay", torch.tensor(exponential_decay))
@@ -25,8 +28,7 @@ class MovingAverageBaseline(Baseline):
     def compute_baseline(
         self, tensor: StochasticTensor, cost_node: CostTensor
     ) -> torch.Tensor:
-        # TODO: Use reduce instead of mean
-        avg_cost = cost_node.detach_tensor().mean().detach()
+        avg_cost = storch.reduce_plates(cost_node).detach()
         self.moving_average = (
             self.exponential_decay * self.moving_average
             + (1 - self.exponential_decay) * avg_cost
@@ -36,21 +38,7 @@ class MovingAverageBaseline(Baseline):
 
 class BatchAverageBaseline(Baseline):
     # Uses the means of the other samples
-    def compute_baseline(
-        self, tensor: StochasticTensor, costs: CostTensor
-    ) -> torch.Tensor:
-        if tensor.n == 1:
-            raise ValueError(
-                "Can only use the batch average baseline if multiple samples are used."
-            )
-        costs = costs.detach()
-        sum_costs = storch.sum(costs, tensor.name)
-        baseline = (sum_costs - costs) / (tensor.n - 1)
-        return baseline
-
-
-class BatchExclusionBaseline(Baseline):
-    # Uses the means of the other samples, as long as they are different
+    # TODO: Doesn't currently supported non-mean weighted plates.
     def compute_baseline(
         self, tensor: StochasticTensor, costs: CostTensor
     ) -> torch.Tensor: