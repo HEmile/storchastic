Fixed calculation error in unordered set estimator

storch/method/unordered.py
@@ -38,10 +38,11 @@ class UnorderedSetEstimator(SampleWithoutReplacementMethod):
         # Compute integration points for the trapezoid rule: v should range from 0 to 1, where both v=0 and v=1 give a value of 0.
         # As the computation happens in log-space, take the logarithm of the result.
         # N
-        log_v = (
+        v = (
             torch.arange(1, self.num_int_points, out=log_probs._tensor.new())
             / self.num_int_points
-        ).log()
+        )
+        log_v = v.log()
 
         # Compute log(1-v^{exp(log_probs+a)}) in a numerically stable way in log-space
         # Uses the gumbel_log_survival function from
@@ -52,10 +53,11 @@ class UnorderedSetEstimator(SampleWithoutReplacementMethod):
             + self.a
             + torch.log(-log_v)[log_probs.plate_dims * (None,) + (slice(None),)]
         )
+
         # Gumbel log survival: log P(g > g_bound) = log(1 - exp(-exp(-g_bound))) for standard gumbel g
         # If g_bound >= 10, use the series expansion for stability with error O((e^-10)^6) (=8.7E-27)
         # See https://www.wolframalpha.com/input/?i=log%281+-+exp%28-y%29%29
-        y = torch.exp(-g_bound)
+        y = torch.exp(g_bound)
         # plates_w_k x N
         terms = torch.where(
             g_bound >= 10, -g_bound - y / 2 + y ** 2 / 24 - y ** 4 / 2880, log1mexp(y)
@@ -74,7 +76,7 @@ class UnorderedSetEstimator(SampleWithoutReplacementMethod):
             * log_v[phi_D_min_S.plate_dims * (None,) + (slice(None),)]
         )
 
-        # Subtract one factor for element that is left out in R
+        # Subtract one term the for element that is left out in R
         # Automatically unsqueezes correctly using plate dimensions
         # plates_w_k x N
         integrand_without_s = integrand - terms